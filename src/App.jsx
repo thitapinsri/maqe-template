daa0bb1e73448c57f37cc7419dd12bbe838ffa72
Thita Pinsri <thitapinsri@gmail.com>
2022-08-26 06:57:49 +0700

    clean code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import PostList from './component/PostList'
-// import axios from "axios";
 const authorsLink = 'http://maqe.github.io/json/authors.json'
 const postsLink = 'http://maqe.github.io/json/posts.json'
 
@@ -11,20 +10,16 @@ const App = () => {
 
   //  get authors
   const getAuthors = async () => {
-    // const response = await fetch(authorsLink)
-    // const data = await response.json()
-    // console.log(data)
-    const response = await axios.get(authorsLink)
-    setAuthors(response.data)
+    const response = await fetch(authorsLink)
+    const data = await response.json()
+    setAuthors(data)
   };
 
   //  get posts
   const getPosts = async () => {
-    // const response = await fetch(postsLink)
-    // const data = await response.json()
-    // console.log(data)
-    const response = await axios.get(postsLink)
-    setPosts(response.data)
+    const response = await fetch(postsLink)
+    const data = await response.json()
+    setPosts(data)
   };
 
   useEffect(() => {
